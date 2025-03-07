5df5db0cf34928b829db085676768fcb477a8c6e
Ehoneah-Frank <ehoneahfrank2017@gmail.com>
2025-03-07 19:25:23 +0000

    updates

--- src/routes/partnershipRoute.js
+++ src/routes/partnershipRoute.js
@@ -4,7 +4,7 @@ import {contactController} from '../controllers/partnershipContactController.js'
 const partnershipRouter = express.Router();
 
 // Submit contact form
-partnershipRouter.post("/contact", contactController.submitContact);
+partnershipRouter.post("/", contactController.submitContact);
 
 
 export default partnershipRouter;
